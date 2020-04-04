01b04e95ccdbab75a99ca46fc08b0ff9b91b1631
ac <derocon@gmail.com>
2020-04-04 16:05:54 +0530

    fre

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
+const fkra = require('./algorithms/fkra');
+const fre = require('./algorithms/fre')
 
-module.exports = function contentChecker(text, callback) {
-    null;
+module.exports = function contentChecker(text) {
+    var result = { fkra: null, fre: null }
+    result.fkra = fkra(text);
+    result.fre = fre(text)
+    return result
 }
